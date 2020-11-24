4494eb38a0f608d98346234c6ee92cdccdc6101b
Tyler Hueter <tylerhueter08@gmail.com>
2020-11-24 10:09:00 -0500

    added templates for home and game play screens

---
+++ smg-client/src/pages/GamePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const GamePage = () => {
+
+
+    return (
+        <div>
+            <h1>I'm Where You Play The Markets</h1>
+        </div>
+    );
+}
+
+export default GamePage;
