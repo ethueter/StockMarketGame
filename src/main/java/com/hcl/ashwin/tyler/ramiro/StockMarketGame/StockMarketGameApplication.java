14ef0834392777736f9b7bda8a48c433dc95657f
Tyler Hueter <tylerhueter08@gmail.com>
2020-11-30 13:03:00 -0500

    adding JWT authentication to server side

--- src/main/java/com/hcl/ashwin/tyler/ramiro/StockMarketGame/StockMarketGameApplication.java
+++ src/main/java/com/hcl/ashwin/tyler/ramiro/StockMarketGame/StockMarketGameApplication.java
@@ -2,8 +2,10 @@ package com.hcl.ashwin.tyler.ramiro.StockMarketGame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+		(exclude = {DataSourceAutoConfiguration.class })
 public class StockMarketGameApplication {
 
 	public static void main(String[] args) {
