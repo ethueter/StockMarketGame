0fde5faea183582feeb9c65aba16a8236d0fa379
Tyler Hueter <tylerhueter08@gmail.com>
2020-12-01 14:00:05 -0500

    home page and leaderboard

--- smg-client/src/pages/Home.js
+++ smg-client/src/pages/Home.js
@@ -3,20 +3,22 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import ToolBar from '@material-ui/core/Toolbar';
-
+import HomeContainer from '../components/HomeContainer';
 
 const Home = () => {
     
 
     return (
-        <div>
-            <AppBar>
-                <ToolBar>
-                    <Button>Logout</Button>
-                </ToolBar>
-            </AppBar>
-            
-        </div>
+      <div>
+        <AppBar  color='inherit' position='relative'>
+          <ToolBar className="toolbar">
+              
+            <Button>Logout</Button>
+                    <Button>Play Game</Button>
+          </ToolBar>
+        </AppBar>
+        <HomeContainer />
+      </div>
     );
 }
 
