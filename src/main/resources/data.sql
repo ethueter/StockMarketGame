53a815c24b8be946d371d8225629f6f2caca7b81
AshwinAzhagiri <ashwin.azhagiri@hcl.com>
2020-12-02 15:53:49 -0500

    Fixed Data.sql table initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,3 @@
-/*
-create table users
-(
-	id integer(5) NOT NULL,
-	username varchar(45),
-	password varchar(45),
-	games integer,
-	created timestamp(0),
-	last_login_date date DEFAULT GETDATE(),
-	archived boolean DEFAULT 0,
-	primarykey(id)
-);
-*/
-INSERT INTO USERS (id, username, password, games)
-VALUES (10000, 'Ashwin', 'pwd', 0);
-
-INSERT INTO USERS (id, username, password, games)
-VALUES (10001, 'Tyler', '123', 2);
-INSERT INTO USERS (id, username, password, games)
-VALUES (10003, 'Player3', '000', 4);
+INSERT INTO USERS (ID, USERNAME, PASSWORD, GAMES, CREATED, LAST_LOGIN_DATE, ARCHIVED) VALUES (10000, 'Ashwin', 'pwd', 0, '2020-12-02 13:40:31.919', '2020-12-02 13:40:31.919', FALSE);
+INSERT INTO USERS (ID, USERNAME, PASSWORD, GAMES, CREATED, LAST_LOGIN_DATE, ARCHIVED) VALUES (10001, 'Tyler', '123', 2, '2020-12-02 13:40:31.919', '2020-12-02 13:40:31.919', FALSE);
+INSERT INTO USERS (ID, USERNAME, PASSWORD, GAMES, CREATED, LAST_LOGIN_DATE, ARCHIVED) VALUES (10003, 'Player3', '000', 4, '2020-12-02 13:40:31.919', '2020-12-02 13:40:31.919', FALSE);
