1085e726be442fa8bbf0c771e3fdc14c69a79b52
Tyler Hueter <tylerhueter08@gmail.com>
2020-12-03 22:35:05 -0500

    add game clock and game mode selector

--- smg-client/src/pages/GamePage.js
+++ smg-client/src/pages/GamePage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Button, Link, Grid } from '@material-ui/core';
+import { AppBar, Button, Link, Grid, InputLabel, Select, MenuItem } from '@material-ui/core';
 import ToolBar from '@material-ui/core/Toolbar';
-
+import useinterval from 'react-useinterval';
 import Porfolio from '../components/Porfolio';
 import TradingDesk from '../components/TradingDesk';
 import TradingFloor from '../components/TradingFloor';
 import stocks from '../testData/stocks';
 import Market from '../services/marketData';
+import Counter from '../components/Counter';
 
 
 const GamePage = () => {
@@ -16,7 +17,7 @@ const GamePage = () => {
   const [mktCall, setMktCall ] = useState();
   const [gameMode, setGameMode ] = useState(15);
 
-  const [sec, setSec ] = useState();
+  const [sec, setSec ] = useState(0);
   const [gameClock, setGameClock] = useState(gameMode);
   const [gameOn, setGameOn ] = useState(false)
 
@@ -27,7 +28,8 @@ const GamePage = () => {
   }, [])
 
   const testingEffect = () => {
-    console.log(mktCall["Meta Data"]["2. Symbol"]);
+    // console.log(mktCall["Meta Data"]["2. Symbol"]);
+    setGameOn(true);
   }
 
   const buyStock = (stk, shares) => {
@@ -41,6 +43,13 @@ const GamePage = () => {
   const getPortVal = () => {
     //needs to establish portfolio
   }
+
+  const handleGameMode = (event) => {
+    console.log(event.target.value);
+    setGameMode(event.target.value);
+    setGameClock(event.target.value);
+  }
+
   const leadingZeros = (num) => {
     if (num === 60 || num === 0) {
       return "00";
@@ -53,7 +62,7 @@ const GamePage = () => {
 
   const gameTimer = () => {
 
-    if (sec === 0 && display === 0) {
+    if (sec === 0 && gameClock === 0) {
       endGame();
     } else if (sec === 0 && gameClock !== 0) {
       setGameClock(min => min - 1);
@@ -78,25 +87,45 @@ const GamePage = () => {
             <Link component={Button} href="/">
               Logout
             </Link>
-            <Link component={Button} href="/home">Home</Link>
+            <Link component={Button} href="/home">
+              Home
+            </Link>
             <Counter
               displayTime={gameClock}
               displaySec={sec}
               zero={leadingZeros}
-               />
+            />
+            <span>
+              <InputLabel id="stock-symbol-lable">Game Mode</InputLabel>
+              <Select
+                labelId="stock-symbol-label"
+                id="stock-symbol"
+                value={gameMode}
+                onChange={handleGameMode}
+              >
+                <MenuItem value="15">15 min</MenuItem>
+                <MenuItem value="30">30 min</MenuItem>
+                <MenuItem value="60">1 hr</MenuItem>
+              </Select>
+            </span>
           </ToolBar>
         </AppBar>
         <Button onClick={testingEffect}>Test</Button>
-        <Grid container >
-            <Grid item md={4}>
-                    <TradingDesk stocks={stocks} buy={buyStock} sell={sellStock} cash={cash} portVal={portValue} />
-            </Grid>
-            <Grid item md={8}>
-                    <TradingFloor stocks={stocks}/>
-            </Grid>
+        <Grid container>
+          <Grid item md={4}>
+            <TradingDesk
+              stocks={stocks}
+              buy={buyStock}
+              sell={sellStock}
+              cash={cash}
+              portVal={portValue}
+            />
+          </Grid>
+          <Grid item md={8}>
+            <TradingFloor stocks={stocks} />
+          </Grid>
         </Grid>
-        
-        
+
         <Porfolio />
       </div>
     );
