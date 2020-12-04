dda3bbe6b108b6e5c8c909cdf44dacf4b6a61c80
AshwinAzhagiri <ashwin.azhagiri@hcl.com>
2020-12-04 10:52:26 -0500

    Game requests
    Mapped leaderboard and new game methods.

--- src/main/java/com/hcl/StockMarketGame/controller/MainController.java
+++ src/main/java/com/hcl/StockMarketGame/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.hcl.StockMarketGame.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +18,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hcl.StockMarketGame.model.Game;
+import com.hcl.StockMarketGame.model.Gamemode;
 import com.hcl.StockMarketGame.model.User;
+import com.hcl.StockMarketGame.service.GameService;
 import com.hcl.StockMarketGame.service.UserService;
 
 @Controller
 public class MainController {
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	GameService gameService;
 	
 	//@CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(value="/login/{username}")
@@ -41,7 +50,7 @@ public class MainController {
 		return userService.getAll();
 	}
     
-    @GetMapping(value="/create/{username}")
+    @PostMapping(value="/create/{username}")
     public @ResponseBody int newUser(@PathVariable String username,@RequestBody String password) {
 //    	if(userService.exists(username)) {
 //    		return 0;
@@ -52,5 +61,23 @@ public class MainController {
     		userService.post(x);
     		return 1;
 //    	}
+    }
+    
+    @GetMapping(value="/newgame/{userId}/{score}/{gameMode}")
+    public @ResponseBody void newGame(@PathVariable("userId") int userId, @PathVariable("score") int score, @PathVariable("gameMode") String gameMode) {
+    	Gamemode gm = Enum.valueOf(Gamemode.class, gameMode);
+    	gameService.post(new Game(userId,score,gm));
+    }
+    
+    @GetMapping(value="/leaderboard")
+    public @ResponseBody List<List<Game>> leaderboard(){
+    	List<List<Game>> lbd = new ArrayList<List<Game>>();
+    	for(Gamemode gm : Gamemode.values()) {
+    		List<Game> games = gameService.get(gm);
+    		Collections.sort(games, (x,y) -> y.getScore()-x.getScore());
+    		lbd.add(games);
+    	}
+    	System.out.println(lbd);
+    	return lbd;
     }
 }
