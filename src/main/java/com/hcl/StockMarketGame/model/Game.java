e8319c77d71c25454f1b8135fd30aa533c7262e8
AshwinAzhagiri <ashwin.azhagiri@hcl.com>
2020-12-07 10:14:01 -0500

    Update Game.java

--- src/main/java/com/hcl/StockMarketGame/model/Game.java
+++ src/main/java/com/hcl/StockMarketGame/model/Game.java
@@ -19,15 +19,15 @@ import java.util.Comparator;
 @Table(name="games")
 public class Game {// Table to store data on every game played
     public Game(){}
-    public Game(int userId, int score, Gamemode gameMode){
-    	this.userId = userId;
+    public Game(String username, int score, Gamemode gameMode){
+    	this.username = username;
     	this.score = score;
     	this.gameMode = gameMode;
     }
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
-    private int userId;
+    private String username;
     @CreationTimestamp
     private Timestamp timestamp;
     private int score;
@@ -41,6 +41,6 @@ public class Game {// Table to store data on every game played
 	}
 	@Override
 	public String toString() {
-		return this.userId + "|score=" + this.score + "|mode=" + this.gameMode;
+		return this.username + "|score=" + this.score + "|mode=" + this.gameMode;
 	}
 }
